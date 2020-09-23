ea7b6ae819d165c74db376902d67ff8b44aa8d25
Zhenqin Wu <zhenqin.wu2@gmail.com>
2020-09-23 16:28:25 +0800

    Update run_save_cross.py

--- scripts/run_save_cross.py
+++ scripts/run_save_cross.py
@@ -14,19 +14,34 @@ w_filenames = [os.path.join(data_path, 'permuted_w_%d.pkl' % i) for i in range(n
 name_file = os.path.join(data_path, 'permuted_names.pkl')
 label_file = os.path.join(data_path, 'permuted_labels.pkl')
 
+kwargs = {
+    'batch_size': 16,
+    'shuffle_inds': False,
+    'include_day': True,
+    'n_segment_classes': 2,
+    'segment_class_weights': [1, 3],
+    'segment_extra_weights': enhance_weight_fp,
+    'segment_label_type': 'segmentation',
+    'n_classify_classes': 2,
+    'classify_class_weights': [0.02, 0.02],
+    'classify_label_fn': binarized_fluorescence_label
+}
+
 gen = CustomGenerator(X_filenames,
                       y_filenames,
                       w_filenames,
                       name_file,
-                      label_file,
-                      include_day=True,
-                      batch_size=8,
-                      n_classify_classes=2,
-                      classify_class_weights=[1, 1],
-                      extra_weights=enhance_weight_fp,
-                      allow_size=50)
+                      label_file=label_file,
+                      augment=False,
+                      allow_size=50,
+                      **kwargs)
 
 out_data_path = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/linear_aligned_patches/cross_7-to-10/'
 if not os.path.exists(out_data_path):
     os.mkdir(out_data_path)
 gen.cross_pair_save(time_interval=[7, 10], seed=123, save_path=out_data_path)
+
+out_data_path = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/linear_aligned_patches/cross_11-to-14/'
+if not os.path.exists(out_data_path):
+    os.mkdir(out_data_path)
+gen.cross_pair_save(time_interval=[11, 14], seed=123, save_path=out_data_path)
