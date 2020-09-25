23e6d8feb716380f26ab774ab1c0c85766eb1873
Zhenqin Wu <zhenqin.wu2@gmail.com>
2020-09-26 00:31:02 +0800

    Update metric_breakdown.py

--- scripts/metric_breakdown.py
+++ scripts/metric_breakdown.py
@@ -51,96 +51,96 @@ model.load('./model_save/pspnet_random_0-to-10_0.model')
 
 ### Prec, Recall, F1 score w.r.t. predict day (from) ###
 
-# classify_y_preds = {}
-# classify_y_trues = {}
-# tp = {}
-# fp = {}
-# fn = {}
-# total_ct = {}
-# thr = 0.01 * (288 * 384)
+classify_y_preds = {}
+classify_y_trues = {}
+tp = {}
+fp = {}
+fn = {}
+total_ct = {}
+thr = 0.01 * (288 * 384)
 
-# for batch in valid_gen:
-#     day = batch[0][..., 1][:, 0, 0]
-#     for d in day:
-#         if not d in classify_y_trues:
-#             classify_y_trues[d] = []
-#             classify_y_preds[d] = []
-#             tp[d] = 0
-#             fp[d] = 0
-#             fn[d] = 0
-#             total_ct[d] = 0
+for batch in valid_gen:
+    day = batch[0][..., 1][:, 0, 0]
+    for d in day:
+        if not d in classify_y_trues:
+            classify_y_trues[d] = []
+            classify_y_preds[d] = []
+            tp[d] = 0
+            fp[d] = 0
+            fn[d] = 0
+            total_ct[d] = 0
 
-#     y_pred, y_pred_classify = model.model.predict(batch[0])
-#     yw_true, yw_true_classify = batch[1]
-#     y_pred = scipy.special.softmax(y_pred, -1)
-#     y_pred_classify = scipy.special.softmax(y_pred_classify, -1)
+    y_pred, y_pred_classify = model.model.predict(batch[0])
+    yw_true, yw_true_classify = batch[1]
+    y_pred = scipy.special.softmax(y_pred, -1)
+    y_pred_classify = scipy.special.softmax(y_pred_classify, -1)
     
-#     y_true = yw_true[..., :-1]
-#     w = yw_true[..., -1]
-#     y_true_classify = yw_true_classify[..., :-1]
-#     w_true_classify = yw_true_classify[..., -1]
+    y_true = yw_true[..., :-1]
+    w = yw_true[..., -1]
+    y_true_classify = yw_true_classify[..., :-1]
+    w_true_classify = yw_true_classify[..., -1]
 
 
-#     classify_valid_inds = np.nonzero(w_true_classify)[0]
-#     for i in classify_valid_inds:
-#         d = day[i]
-#         classify_y_trues[d].append(y_true_classify[i])
-#         classify_y_preds[d].append(y_pred_classify[i])
+    classify_valid_inds = np.nonzero(w_true_classify)[0]
+    for i in classify_valid_inds:
+        d = day[i]
+        classify_y_trues[d].append(y_true_classify[i])
+        classify_y_preds[d].append(y_pred_classify[i])
 
-#     assert y_pred.shape[0] == y_true.shape[0] == w.shape[0]
-#     for _y_pred, _y_true, _w, d in zip(y_pred, y_true, w, day):
-#       _y_pred = _y_pred[np.nonzero(_w)].reshape((-1, 2))
-#       _y_true = _y_true[np.nonzero(_w)].reshape((-1, 2))
-#       _tp = ((_y_pred[:, 1] > 0.5) * _y_true[:, 1]).sum()
-#       _fp = ((_y_pred[:, 1] > 0.5) * _y_true[:, 0]).sum()
-#       _fn = ((_y_pred[:, 1] <= 0.5) * _y_true[:, 1]).sum()
+    assert y_pred.shape[0] == y_true.shape[0] == w.shape[0]
+    for _y_pred, _y_true, _w, d in zip(y_pred, y_true, w, day):
+      _y_pred = _y_pred[np.nonzero(_w)].reshape((-1, 2))
+      _y_true = _y_true[np.nonzero(_w)].reshape((-1, 2))
+      _tp = ((_y_pred[:, 1] > 0.5) * _y_true[:, 1]).sum()
+      _fp = ((_y_pred[:, 1] > 0.5) * _y_true[:, 0]).sum()
+      _fn = ((_y_pred[:, 1] <= 0.5) * _y_true[:, 1]).sum()
 
-#       tp[d] += _tp
-#       fp[d] += _fp
-#       fn[d] += _fn
-#       total_ct[d] += 1
+      tp[d] += _tp
+      fp[d] += _fp
+      fn[d] += _fn
+      total_ct[d] += 1
 
 
-# iou = {}
-# prec = {}
-# recall = {}
-# f1 = {}
-# for d in tp:
-#     iou[d] = tp[d]/(tp[d] + fp[d] + fn[d])
-#     prec[d] = tp[d]/(tp[d] + fp[d])
-#     recall[d] = tp[d]/(tp[d] + fn[d])
-#     f1[d] = 2/(1/(prec[d] + 1e-5) + 1/(recall[d] + 1e-5))
+iou = {}
+prec = {}
+recall = {}
+f1 = {}
+for d in tp:
+    iou[d] = tp[d]/(tp[d] + fp[d] + fn[d])
+    prec[d] = tp[d]/(tp[d] + fp[d])
+    recall[d] = tp[d]/(tp[d] + fn[d])
+    f1[d] = 2/(1/(prec[d] + 1e-5) + 1/(recall[d] + 1e-5))
 
-# x = sorted(f1.keys())
-# plt.clf()
-# plt.plot(x, [f1[_x] for _x in x], '.-', label='segmentation f1')
-# plt.plot(x, [prec[_x] for _x in x], '.-', label='segmentation precision')
-# plt.plot(x, [recall[_x] for _x in x], '.-', label='segmentation recall')
-# plt.xlabel('Day (from)')
-# plt.legend()
-# plt.savefig('/home/zqwu/Dropbox/fig_temp/seg_0-to-10.png', dpi=300)
+x = sorted(f1.keys())
+plt.clf()
+plt.plot(x, [f1[_x] for _x in x], '.-', label='segmentation f1')
+plt.plot(x, [prec[_x] for _x in x], '.-', label='segmentation precision')
+plt.plot(x, [recall[_x] for _x in x], '.-', label='segmentation recall')
+plt.xlabel('Day (from)')
+plt.legend()
+plt.savefig('/home/zqwu/Dropbox/fig_temp/seg_0-to-10.png', dpi=300)
 
-# c_auc = {}
-# c_prec = {}
-# c_recall = {}
-# c_f1 = {}
-# for d in classify_y_trues:
-#     classify_y_trues[d] = np.stack(classify_y_trues[d], 0)
-#     classify_y_preds[d] = np.stack(classify_y_preds[d], 0)
-#     c_auc[d] = roc_auc_score(classify_y_trues[d], classify_y_preds[d])
-#     c_prec[d] = precision_score(classify_y_trues[d][:, 1], classify_y_preds[d][:, 1] > 0.5)
-#     c_recall[d] = recall_score(classify_y_trues[d][:, 1], classify_y_preds[d][:, 1] > 0.5)
-#     c_f1[d] = f1_score(classify_y_trues[d][:, 1], classify_y_preds[d][:, 1] > 0.5)
+c_auc = {}
+c_prec = {}
+c_recall = {}
+c_f1 = {}
+for d in classify_y_trues:
+    classify_y_trues[d] = np.stack(classify_y_trues[d], 0)
+    classify_y_preds[d] = np.stack(classify_y_preds[d], 0)
+    c_auc[d] = roc_auc_score(classify_y_trues[d], classify_y_preds[d])
+    c_prec[d] = precision_score(classify_y_trues[d][:, 1], classify_y_preds[d][:, 1] > 0.5)
+    c_recall[d] = recall_score(classify_y_trues[d][:, 1], classify_y_preds[d][:, 1] > 0.5)
+    c_f1[d] = f1_score(classify_y_trues[d][:, 1], classify_y_preds[d][:, 1] > 0.5)
 
-# x = sorted(c_f1.keys())
-# plt.clf()
-# plt.plot(x, [c_f1[_x] for _x in x], '.-', label='segmentation f1')
-# plt.plot(x, [c_prec[_x] for _x in x], '.-', label='segmentation precision')
-# plt.plot(x, [c_recall[_x] for _x in x], '.-', label='segmentation recall')
-# plt.plot(x, [c_auc[_x] for _x in x], '.-', label='segmentation roc-auc')
-# plt.xlabel('Day (from)')
-# plt.legend()
-# plt.savefig('/home/zqwu/Dropbox/fig_temp/cla_0-to-10.png', dpi=300)
+x = sorted(c_f1.keys())
+plt.clf()
+plt.plot(x, [c_f1[_x] for _x in x], '.-', label='segmentation f1')
+plt.plot(x, [c_prec[_x] for _x in x], '.-', label='segmentation precision')
+plt.plot(x, [c_recall[_x] for _x in x], '.-', label='segmentation recall')
+plt.plot(x, [c_auc[_x] for _x in x], '.-', label='segmentation roc-auc')
+plt.xlabel('Day (from)')
+plt.legend()
+plt.savefig('/home/zqwu/Dropbox/fig_temp/cla_0-to-10.png', dpi=300)
 
 
 ### Accuracy of 4 class w.r.t. predict day (from) ###
@@ -257,7 +257,7 @@ plt.savefig('/home/zqwu/Dropbox/fig_temp/pred_distri_from1.png', dpi=300)
 
 plt.clf()
 fig, ax = plt.subplots(figsize=(7, 1.5))
-ax.plot(x, [len(pred_1_1[_x]) for _x in selected_x], c='lightgreen')
+ax.plot(x, [len(pred_1_1[_x]) for _x in x], c='lightgreen')
 ax.set_xticks(x)
 ax.set_xlim([5, 17])
 ax.set_ylim([0, ax.get_ylim()[1]])
