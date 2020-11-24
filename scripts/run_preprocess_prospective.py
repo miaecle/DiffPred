d4b90b9e5c5836a2b26c313d04ebc0b104087d8c
Zhenqin Wu <zhenqin.wu2@gmail.com>
2020-11-24 22:29:48 +0800

    Update run_preprocess_prospective.py

--- scripts/run_preprocess_prospective.py
+++ scripts/run_preprocess_prospective.py
@@ -1,23 +1,24 @@
 import os
 import csv
+import pickle
+import cv2
 import numpy as np
 from data_generator import binarized_fluorescence_label
-from data_loader import *
-from segment_support import *
-
+from data_loader import load_image_pair, get_all_files, get_ex_day, get_well
+from segment_support import preprocess, assemble_for_training, generate_fluorescence_labels
 
 def load_assemble_test_data(data_path, dataset_path):
     # Load data under the given path
-    print("Loading Data")
+    print("Loading Data", flush=True)
     fs = get_all_files(data_path)
     fs = [f for f in fs if not str(get_well(f)[1]) in ['1', '2', '16', '14', '15', '30', '196', '211', '212', '210', '224', '225']]
     fs = [f for f in fs if 'Phase' in f]
     fs_pair = [(f, None) for f in fs]
-    print("Number of input images: %d" % len(fs_pair))
+    print("Number of input images: %d" % len(fs_pair), flush=True)
     pair_dats = {pair: load_image_pair(pair) for pair in fs_pair}
 
     # Preprocessing
-    print("Start Preprocessing")
+    print("Start Preprocessing", flush=True)
     if not os.path.exists(dataset_path):
         os.makedirs(dataset_path)
 
@@ -32,19 +33,21 @@ def load_assemble_test_data(data_path, dataset_path):
 def load_test_data_labels(data_path, label_save_path):
     fs = get_all_files(data_path)
     ex, day = get_ex_day(data_path)
-    fs = [f for f in fs if not get_well(f)[1] in ['1', '2', '16', '14', '15', '30', '196', '211', '212', '210', '224', '225']]
+    fs = [f for f in fs if not str(get_well(f)[1]) in ['1', '2', '16', '14', '15', '30', '196', '211', '212', '210', '224', '225']]
     pc_fs = [f for f in fs if 'Phase' in f]
     fp_fs = [f for f in fs if 'FP' in f]
+
     assert len(pc_fs) == len(fp_fs)
     fs_pair = list(zip(pc_fs, fp_fs))
     pair_dats = {pair: load_image_pair(pair) for pair in fs_pair}
 
     fl_labels = {}
+    labels = {}
     binarized_labels = {}
     target_size = (384, 288)
     for i, pair in enumerate(fs_pair):
         if i%100 == 0:
-            print(i)
+            print(i, flush=True)
         key = get_ex_day(pair[0]) + get_well(pair[0])
         if key in fl_labels:
             continue
@@ -75,17 +78,17 @@ def load_test_data_labels(data_path, label_save_path):
 
 
 if __name__ == '__main__':
-    folder_paths = os.listdir('/scratch/users/zqwu/iPSC_data/prospective/cms/ex1/')
-    label_save_path = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/prospective/ex1/'
+    folder_paths = os.listdir('/scratch/users/zqwu/iPSC_data/prospective/cms/ex2/')
+    label_save_path = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/prospective/ex2/'
     if not os.path.exists(label_save_path):
         os.makedirs(label_save_path)
     for folder in sorted(folder_paths):
         print(folder, flush=True)
-        raw_path = '/scratch/users/zqwu/iPSC_data/prospective/cms/ex1/%s' % folder
-        save_path = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/prospective/ex1/%s/' % folder
+        raw_path = '/scratch/users/zqwu/iPSC_data/prospective/cms/ex2/%s' % folder
+        save_path = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/prospective/ex2/%s/' % folder
 
         ex, day = get_ex_day(raw_path)
         _ = load_assemble_test_data(raw_path, save_path)
 
-        if int(day[1:]) >= 8 and int(day[1:]) <= 17:
+        if int(day[1:]) >= 8:
             load_test_data_labels(raw_path, label_save_path)
