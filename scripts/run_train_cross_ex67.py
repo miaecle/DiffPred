b3f9cfa34b37744a8ed1ab00f604eeeaf923ebed
Zhenqin Wu <zhenqin.wu2@gmail.com>
2020-11-25 22:51:01 +0800

    Add another train script

--- scripts/run_train_cross_ex67.py
+++ scripts/run_train_cross_ex67.py
@@ -10,26 +10,11 @@ from scipy.stats import spearmanr, pearsonr
 
 
 ROOT_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/linear_aligned_patches/'
-MODEL_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/model_save/random/'
+MODEL_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/model_save/ex67/'
 
 if not os.path.exists(MODEL_DIR):
   os.makedirs(MODEL_DIR)
 
-"""
-names = pickle.load(open(os.path.join(ROOT_DIR, 'merged_all', 'permuted_names.pkl'), 'rb'))
-unique_wells = sorted(set(get_ex_day(n)[:1] + get_well(n) for n in names.values()))
-np.random.seed(123)
-np.random.shuffle(unique_wells)
-valid_wells = set(unique_wells[:int(0.2*len(unique_wells))])
-train_wells = set(unique_wells[int(0.2*len(unique_wells)):])
-with open(os.path.join(ROOT_DIR, 'merged_all', 'random_train_wells.pkl'), 'wb') as f:
-  pickle.dump(train_wells, f)
-with open(os.path.join(ROOT_DIR, 'merged_all', 'random_valid_wells.pkl'), 'wb') as f:
-  pickle.dump(valid_wells, f)
-"""
-
-train_wells = pickle.load(open(os.path.join(ROOT_DIR, 'merged_all', 'random_train_wells.pkl'), 'rb'))
-valid_wells = pickle.load(open(os.path.join(ROOT_DIR, 'merged_all', 'random_valid_wells.pkl'), 'rb'))
 
 data_path = os.path.join(ROOT_DIR, 'cross_infinite')
 n_fs = len([f for f in os.listdir(data_path) if f.startswith('X')])
@@ -40,10 +25,8 @@ name_file = os.path.join(data_path, 'names.pkl')
 label_file = os.path.join(data_path, 'labels.pkl')
 
 cross_names = pickle.load(open(name_file, 'rb'))
-valid_inds = [i for i, n in cross_names.items() if get_ex_day(n[0])[:1] + get_well(n[0]) in valid_wells]
-train_inds = [i for i, n in cross_names.items() if get_ex_day(n[0])[:1] + get_well(n[0]) in train_wells]
-# train_inds = np.array([i for i, n in cross_names.items() if not get_ex_day(n[0])[0] == 'ex1'])
-# valid_inds = np.array([i for i, n in cross_names.items() if get_ex_day(n[0])[0] == 'ex1'])
+train_inds = np.array([i for i, n in cross_names.items() if not get_ex_day(n[0])[0] in ['ex6', 'ex7']])
+valid_inds = np.array([i for i, n in cross_names.items() if get_ex_day(n[0])[0] in ['ex6', 'ex7']])
 print(len(train_inds))
 print(len(valid_inds))
 
@@ -68,13 +51,13 @@ train_gen = PairGenerator(X_filenames,
                           selected_inds=train_inds,
                           **kwargs)
 
-# valid_filenames = train_gen.reorder_save(valid_inds, save_path=os.path.join(data_path, 'random_valid_'))
-n_fs = len([f for f in os.listdir(data_path) if f.startswith('random_valid_X')])
-X_filenames = [os.path.join(data_path, 'random_valid_X_%d.pkl' % i) for i in range(n_fs)]
-y_filenames = [os.path.join(data_path, 'random_valid_y_%d.pkl' % i) for i in range(n_fs)]
-w_filenames = [os.path.join(data_path, 'random_valid_w_%d.pkl' % i) for i in range(n_fs)]
-name_file = os.path.join(data_path, 'random_valid_names.pkl')
-label_file = os.path.join(data_path, 'random_valid_labels.pkl')
+valid_filenames = train_gen.reorder_save(valid_inds, save_path=os.path.join(data_path, 'ex67_'))
+n_fs = len([f for f in os.listdir(data_path) if f.startswith('ex67_X')])
+X_filenames = [os.path.join(data_path, 'ex67_X_%d.pkl' % i) for i in range(n_fs)]
+y_filenames = [os.path.join(data_path, 'ex67_y_%d.pkl' % i) for i in range(n_fs)]
+w_filenames = [os.path.join(data_path, 'ex67_w_%d.pkl' % i) for i in range(n_fs)]
+name_file = os.path.join(data_path, 'ex67_names.pkl')
+label_file = os.path.join(data_path, 'ex67_labels.pkl')
 valid_gen = PairGenerator(X_filenames,
                           y_filenames,
                           w_filenames,
@@ -95,4 +78,4 @@ print("Start Training", flush=True)
 model.fit(train_gen,
           valid_gen=valid_gen,
           n_epochs=200)
-model.save(os.path.join(MODEL_DIR, 'pspnet_random_0-to-inf_0.model'))
+model.save(os.path.join(MODEL_DIR, 'pspnet_ex67_0-to-inf_0.model'))
