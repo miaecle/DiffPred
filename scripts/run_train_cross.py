6533d7b47593a8c77fb4378f8419d6c7e0c289d4
Zhenqin Wu <zhenqin.wu2@gmail.com>
2020-11-25 22:22:36 +0800

    Update run_train_cross.py

--- scripts/run_train_cross.py
+++ scripts/run_train_cross.py
@@ -1,5 +1,6 @@
 import os
 os.environ['KERAS_BACKEND'] = 'tensorflow'
+os.environ['SM_FRAMEWORK'] = 'tf.keras'
 from data_loader import *
 from segment_support import *
 from models import Segment, ClassifyOnSegment
@@ -7,10 +8,27 @@ from layers import load_partial_weights, fill_first_layer
 from data_generator import CustomGenerator, PairGenerator, enhance_weight_fp
 from scipy.stats import spearmanr, pearsonr
 
-valid_wells = pickle.load(open('data/linear_aligned_patches/merged_all/random_valid_wells.pkl', 'rb'))
-train_wells = pickle.load(open('data/linear_aligned_patches/merged_all/random_train_wells.pkl', 'rb'))
 
-data_path = 'data/linear_aligned_patches/cross_infinite/'
+ROOT_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/linear_aligned_patches/'
+MODEL_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/model_save/'
+
+"""
+names = pickle.load(open(os.path.join(ROOT_DIR, 'merged_all', 'permuted_names.pkl'), 'rb'))
+unique_wells = sorted(set(get_ex_day(n)[:1] + get_well(n) for n in names.values()))
+np.random.seed(123)
+np.random.shuffle(unique_wells)
+valid_wells = set(unique_wells[:int(0.2*len(unique_wells))])
+train_wells = set(unique_wells[int(0.2*len(unique_wells)):])
+with open(os.path.join(ROOT_DIR, 'merged_all', 'random_train_wells.pkl'), 'wb') as f:
+  pickle.dump(train_wells, f)
+with open(os.path.join(ROOT_DIR, 'merged_all', 'random_valid_wells.pkl'), 'wb') as f:
+  pickle.dump(valid_wells, f)
+"""
+
+train_wells = pickle.load(open(os.path.join(ROOT_DIR, 'merged_all', 'random_train_wells.pkl'), 'rb'))
+valid_wells = pickle.load(open(os.path.join(ROOT_DIR, 'merged_all', 'random_valid_wells.pkl'), 'rb'))
+
+data_path = os.path.join(ROOT_DIR, 'cross_infinite')
 n_fs = len([f for f in os.listdir(data_path) if f.startswith('X')])
 X_filenames = [os.path.join(data_path, 'X_%d.pkl' % i) for i in range(n_fs)]
 y_filenames = [os.path.join(data_path, 'y_%d.pkl' % i) for i in range(n_fs)]
@@ -47,7 +65,7 @@ train_gen = PairGenerator(X_filenames,
                           selected_inds=train_inds,
                           **kwargs)
 
-valid_filenames = train_gen.reorder_save(valid_inds, save_path=data_path+'random_valid_')
+# valid_filenames = train_gen.reorder_save(valid_inds, save_path=os.path.join(data_path, 'random_valid_'))
 n_fs = len([f for f in os.listdir(data_path) if f.startswith('random_valid_X')])
 X_filenames = [os.path.join(data_path, 'random_valid_X_%d.pkl' % i) for i in range(n_fs)]
 y_filenames = [os.path.join(data_path, 'random_valid_y_%d.pkl' % i) for i in range(n_fs)]
@@ -61,17 +79,17 @@ valid_gen = PairGenerator(X_filenames,
                           label_file=label_file,
                           **kwargs)
 
-
+print("Initiate Model", flush=True)
 model = ClassifyOnSegment(
     input_shape=(288, 384, 3),
     model_structure='pspnet',
-    model_path='model_save',
+    model_path=MODEL_DIR,
     encoder_weights='imagenet',
     n_segment_classes=2,
     n_classify_classes=2)
 
-
+print("Start Training", flush=True)
 model.fit(train_gen,
           valid_gen=valid_gen,
-          n_epochs=50)
-model.save('./model_save/pspnet_random_0-to-inf_0.model')
+          n_epochs=2)
+model.save(os.path.join(MODEL_DIR, 'pspnet_random_0-to-inf_0.model'))
