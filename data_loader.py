f8a4cdf4795686fe188c1da1aae6f235d5fe72d6
Zhenqin Wu <zhenqin.wu2@gmail.com>
2021-03-17 11:37:59 +0800

    error case handling

--- data_loader.py
+++ data_loader.py
@@ -102,6 +102,8 @@ def load_image(f):
         img = tifffile.imread(f)
     except Exception as e:
         img = cv2.imread(f, -1)
+        if img is None:
+            return None
         if len(img.shape) == 3:
             img = img[..., 0]
     if img.dtype == 'uint16':
