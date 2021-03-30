f55bac469c34d4597bfc02470e067d407296c954
Zhenqin Wu <zhenqin.wu2@gmail.com>
2021-03-30 18:38:13 +0800

    Update run_train.py

--- scripts/run_train.py
+++ scripts/run_train.py
@@ -1,80 +1,98 @@
 import os
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 os.environ['SM_FRAMEWORK'] = 'tf.keras'
-from data_loader import *
-from segment_support import *
-from layers import *
+import pickle
+import numpy as np
+from data_loader import get_identifier
 from models import Segment, ClassifyOnSegment
-from data_generator import CustomGenerator, enhance_weight_fp, binarized_fluorescence_label
+from layers import load_partial_weights, fill_first_layer, evaluate_confusion_mat
+from data_generator import CustomGenerator, PairGenerator, enhance_weight_for_false_positives
+from scipy.stats import spearmanr, pearsonr
+
+### Settings ###
+ROOT_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/train_set/0-to-0_continuous/'
+VALID_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/train_set/0-to-0_continuous/random_valid/'
+SPLIT_FILE = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/train_set/random_split.pkl'
+
+MODEL_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/model_save/0-to-0_random/'
+os.makedirs(ROOT_DIR, exist_ok=True)
+os.makedirs(VALID_DIR, exist_ok=True)
+os.makedirs(MODEL_DIR, exist_ok=True)
+
+def well_info(name):
+    return get_identifier(name)[:2] + get_identifier(name)[3:]
 
 
-DATA_ROOT = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/'
-data_path = os.path.join(DATA_ROOT, 'linear_aligned_patches', 'merged_all')
-model_path = os.path.join(DATA_ROOT, 'model_save', '0-to-0_random')
-os.makedirs(model_path, exist_ok=True)
+### Train-Valid split ###
+
+# Setting up training set
+n_fs = len([f for f in os.listdir(ROOT_DIR) if f.startswith('X_') and f.endswith('.pkl')])
+X_filenames = [os.path.join(ROOT_DIR, 'X_%d.pkl' % i) for i in range(n_fs)]
+y_filenames = [os.path.join(ROOT_DIR, 'segment_continuous_y_%d.pkl' % i) for i in range(n_fs)]
+w_filenames = [os.path.join(ROOT_DIR, 'segment_continuous_w_%d.pkl' % i) for i in range(n_fs)]
+name_file = os.path.join(ROOT_DIR, 'names.pkl')
+label_file = os.path.join(ROOT_DIR, 'classify_continuous_labels.pkl')
+
+train_wells, valid_wells = pickle.load(open(SPLIT_FILE, 'rb'))
+names = pickle.load(open(name_file, 'rb'))
+valid_inds = [i for i, n in names.items() if well_info(n) in valid_wells]
+train_inds = [i for i, n in names.items() if well_info(n) in train_wells]
+print("N(train): %d" % len(train_inds))
+print("N(valid): %d" % len(valid_inds))
 
 kwargs = {
-    'batch_size': 16,
+    'batch_size': 8,
     'shuffle_inds': False,
     'include_day': True,
-    'n_segment_classes': 2,
-    'segment_class_weights': [1, 3],
-    'segment_extra_weights': enhance_weight_fp,
-    'segment_label_type': 'segmentation',
-    'n_classify_classes': 2,
-    'classify_class_weights': [0.02, 0.02]
+    'n_segment_classes': 4,
+    'segment_class_weights': [1, 1, 1, 1],
+    'segment_extra_weights': None,
+    'segment_label_type': 'continuous',
+    'n_classify_classes': 4,
+    'classify_class_weights': [1, 1, 1, 1],
+    'classify_label_type': 'continuous',
 }
 
-valid_wells = pickle.load(open(os.path.join(DATA_ROOT, 'linear_aligned_patches', 'merged_all', 'random_valid_wells.pkl'), 'rb'))
-train_wells = pickle.load(open(os.path.join(DATA_ROOT, 'linear_aligned_patches', 'merged_all', 'random_train_wells.pkl'), 'rb'))
+train_gen = CustomGenerator(
+    name_file,
+    X_filenames, 
+    segment_y_files=y_filenames, 
+    segment_w_files=w_filenames,
+    classify_label_file=label_file,
+    selected_inds=train_inds,
+    augment=True,
+    **kwargs)
 
-n_fs = len([f for f in os.listdir(data_path) if f.startswith('permuted_X')])
-X_filenames = [os.path.join(data_path, 'permuted_X_%d.pkl' % i) for i in range(n_fs)]
-y_filenames = [os.path.join(data_path, 'permuted_y_%d.pkl' % i) for i in range(n_fs)]
-w_filenames = [os.path.join(data_path, 'permuted_w_%d.pkl' % i) for i in range(n_fs)]
-name_file = os.path.join(data_path, 'permuted_names.pkl')
-label_file = os.path.join(data_path, 'permuted_labels.pkl')
+# Setting up validation set
+valid_filenames = train_gen.reorder_save(valid_inds, save_path=VALID_DIR)
+n_fs = len([f for f in os.listdir(VALID_DIR) if f.startswith('X_') and f.endswith('.pkl')])
+X_filenames = [os.path.join(VALID_DIR, 'X_%d.pkl' % i) for i in range(n_fs)]
+y_filenames = [os.path.join(VALID_DIR, 'segment_continuous_y_%d.pkl' % i) for i in range(n_fs)]
+w_filenames = [os.path.join(VALID_DIR, 'segment_continuous_w_%d.pkl' % i) for i in range(n_fs)]
+name_file = os.path.join(VALID_DIR, 'names.pkl')
+label_file = os.path.join(VALID_DIR, 'classify_continuous_labels.pkl')
 
-names = pickle.load(open(name_file, 'rb'))
-fl_inds = pickle.load(open(os.path.join(data_path, 'permuted_sample_inds_with_fl.pkl'), 'rb'))
-valid_inds = [i for i, n in names.items() if get_ex_day(n)[:1] + get_well(n) in valid_wells and i in fl_inds]
-train_inds = [i for i, n in names.items() if get_ex_day(n)[:1] + get_well(n) in train_wells and i in fl_inds]
-print(len(train_inds))
-print(len(valid_inds))
+valid_gen = CustomGenerator(
+    name_file,
+    X_filenames, 
+    segment_y_files=y_filenames, 
+    segment_w_files=w_filenames,
+    classify_label_file=label_file,
+    **kwargs)
 
-train_gen = CustomGenerator(X_filenames,
-                            y_filenames,
-                            w_filenames,
-                            name_file,
-                            label_file=label_file,
-                            augment=True,
-                            selected_inds=train_inds,
-                            **kwargs)
-
-# valid_filenames = train_gen.reorder_save(valid_inds, save_path=os.path.join(data_path, 'random_valid_'))
-n_fs = len([f for f in os.listdir(data_path) if f.startswith('random_valid_X')])
-X_filenames = [os.path.join(data_path, 'random_valid_X_%d.pkl' % i) for i in range(n_fs)]
-y_filenames = [os.path.join(data_path, 'random_valid_y_%d.pkl' % i) for i in range(n_fs)]
-w_filenames = [os.path.join(data_path, 'random_valid_w_%d.pkl' % i) for i in range(n_fs)]
-name_file = os.path.join(data_path, 'random_valid_names.pkl')
-label_file = os.path.join(data_path, 'random_valid_labels.pkl')
-valid_gen = CustomGenerator(X_filenames,
-                            y_filenames,
-                            w_filenames,
-                            name_file,
-                            label_file=label_file,
-                            **kwargs)
-
+### Training ###
 model = ClassifyOnSegment(
     input_shape=(288, 384, 2), 
     model_structure='pspnet', 
-    model_path=model_path, 
+    model_path=MODEL_DIR, 
     encoder_weights='imagenet',
-    n_segment_classes=2,
-    n_classify_classes=2)
-
-model.load(os.path.join(model_path, 'bkp.model'))
+    n_segment_classes=4,
+    n_classify_classes=4,
+    eval_fn=evaluate_confusion_mat)
 
 model.fit(train_gen,
           valid_gen=valid_gen,
-          n_epochs=50)
+          n_epochs=200,
+          verbose=2)
+model.save(os.path.join(MODEL_DIR, 'pspnet_random_0-to-0_0.model'))
+
