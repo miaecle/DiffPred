cb168371d09588649c730e4c16a0a4e608e3d4a1
Zhenqin Wu <zhenqin.wu2@gmail.com>
2021-05-07 02:00:10 +0800

    Update run_test.py

--- scripts/run_test.py
+++ scripts/run_test.py
@@ -10,7 +10,7 @@ from layers import load_partial_weights, fill_first_layer, evaluate_confusion_ma
 from data_generator import CustomGenerator, PairGenerator, enhance_weight_for_false_positives
 from scipy.stats import spearmanr, pearsonr
 
-MODEL_DIR = "/oak/stanford/groups/jamesz/zqwu/iPSC_data/model_save/random_split/0-to-0_random/"
+MODEL_DIR = "/oak/stanford/groups/jamesz/zqwu/iPSC_data/model_save/random_split/0-to-inf_random/"
 
 model = ClassifyOnSegment(
     input_shape=(288, 384, 3), 
@@ -23,6 +23,29 @@ model = ClassifyOnSegment(
 
 model.load(os.path.join(MODEL_DIR, 'bkp.model'))
 
+def augment_fixed_end(X, end=15):
+    assert X.shape[-1] == 2
+    interval_slice = np.ones_like(X[..., 1:2]) * end - X[..., 1:2]
+    _X = np.concatenate([X, interval_slice], -1)
+    return _X
+
+def augment_fixed_interval(X, interval=15):
+    assert X.shape[-1] == 2
+    interval_slice = np.ones_like(X[..., 1:2]) * interval
+    _X = np.concatenate([X, interval_slice], -1)
+    return _X
+
+input_process_fn = partial(augment_fixed_end, end=15)
+def collect_preds(gen, model, input_process_fn=lambda x: x):
+    gen.batch_with_name = True
+    full_preds = {}
+    for batch in gen:
+        X = input_process_fn(batch[0])
+        ids = [get_identifier(n) for n in batch[-1]]
+        preds = model.predict_on_X(X)
+        for pair in zip(ids, *preds):
+            full_preds[pair[0]] = pair[1:]
+    return full_preds
 
 
 kwargs = {
@@ -38,45 +61,26 @@ kwargs = {
     'classify_label_type': 'discrete',
 }
 
+ROOT_DIRS = [
+    "/oak/stanford/groups/jamesz/zqwu/iPSC_data/validation_set/line2_15_ex1/0-to-0/",
+    "/oak/stanford/groups/jamesz/zqwu/iPSC_data/validation_set/line_142/0-to-0/",
+    "/oak/stanford/groups/jamesz/zqwu/iPSC_data/validation_set/line_20/0-to-0/",
+    "/oak/stanford/groups/jamesz/zqwu/iPSC_data/validation_set/line_diabetes/0-to-0/",
+    "/oak/stanford/groups/jamesz/zqwu/iPSC_data/validation_set/line_LMNA_control/0-to-0/",
+]
 
-ROOT_DIR = "/oak/stanford/groups/jamesz/zqwu/iPSC_data/validation_set/line2_15_ex1/0-to-0/"
-n_fs = len([f for f in os.listdir(ROOT_DIR) if f.startswith('X_') and f.endswith('.pkl')])
-X_filenames = [os.path.join(ROOT_DIR, 'X_%d.pkl' % i) for i in range(n_fs)]
-name_file = os.path.join(ROOT_DIR, 'names.pkl')
+for ROOT_DIR in ROOT_DIRS:
+    n_fs = len([f for f in os.listdir(ROOT_DIR) if f.startswith('X_') and f.endswith('.pkl')])
+    X_filenames = [os.path.join(ROOT_DIR, 'X_%d.pkl' % i) for i in range(n_fs)]
+    name_file = os.path.join(ROOT_DIR, 'names.pkl')
 
-test_gen = CustomGenerator(
-    name_file,
-    X_filenames, 
-    augment=False,
-    batch_with_name=True,
-    **kwargs)
+    test_gen = CustomGenerator(
+        name_file,
+        X_filenames, 
+        augment=False,
+        batch_with_name=True,
+        **kwargs)
 
-
-def augment_fixed_end(X, end=15):
-    assert X.shape[-1] == 2
-    interval_slice = np.ones_like(X[..., 1:2]) * end - X[..., 1:2]
-    _X = np.concatenate([X, interval_slice], -1)
-    return _X
-
-def augment_fixed_interval(X, interval=15):
-    assert X.shape[-1] == 2
-    interval_slice = np.ones_like(X[..., 1:2]) * interval
-    _X = np.concatenate([X, interval_slice], -1)
-    return _X
-
-
-def collect_preds(gen, model, input_process_fn=lambda x: x):
-    gen.batch_with_name = True
-    full_preds = {}
-    for batch in gen:
-        X = input_process_fn(batch[0])
-        ids = [get_identifier(n) for n in batch[-1]]
-        preds = model.predict_on_X(X)
-        for pair in zip(ids, *preds):
-            full_preds[pair[0]] = pair[1:]
-    return full_preds
-
-
-
-input_process_fn = partial(augment_fixed_end, end=15)
-preds = collect_preds(test_gen, model, input_process_fn=input_process_fn)
+    preds = collect_preds(test_gen, model, input_process_fn=input_process_fn)
+    with open(os.path.join(ROOT_DIR, "random_split_0-to-inf_predictions.pkl"), "wb") as f:
+        pickle.dump(preds, f)
