006ab24abddbe76fe882a108e698e15dfe73c3ce
miaecle <zqwu@stanford.edu>
2021-08-30 22:41:31 -0700

    update

--- scripts/collect_predictions.py
+++ scripts/collect_predictions.py
@@ -4,6 +4,7 @@ os.environ['SM_FRAMEWORK'] = 'tf.keras'
 import pickle
 import numpy as np
 import scipy
+import argparse
 import matplotlib.pyplot as plt
 from functools import partial
 from scipy.stats import spearmanr, pearsonr
@@ -102,9 +103,7 @@ def collect_preds(valid_gen,
                   model, 
                   pred_save_dir, 
                   input_transform=None, 
-                  input_filter=None, 
-                  support_gen=None,
-                  support_transform=None):
+                  input_filter=None):
     os.makedirs(pred_save_dir, exist_ok=True)
 
     pred_save = {"seg_preds": [], "seg_trues": [], "seg_ws": [], "pred_names": []}
@@ -113,9 +112,6 @@ def collect_preds(valid_gen,
     cla_trues = []
     cla_ws = []
     pred_names = []
-
-    if not support_gen is None:
-        support_iter = iter(support_gen)
 
     for batch in valid_gen:
         X = batch[0]
@@ -128,16 +124,6 @@ def collect_preds(valid_gen,
             continue
         if not input_transform is None:
             X = input_transform(X)
-
-        if not support_gen is None:
-            try:
-                support_X = next(support_iter)[0]
-            except StopIteration:
-                support_iter = iter(support_gen)
-                support_X = next(support_iter)[0]
-            if not support_transform is None:
-                support_X = support_transform(support_X)
-            X = np.concatenate([X, support_X])
 
         pred = model.model.predict(X)
         seg_pred = scipy.special.softmax(pred[0], -1)
@@ -177,23 +163,44 @@ def collect_preds(valid_gen,
                      "pred_names": pred_names}, f)
 
 
+def parse_args(cli=True):
+    parser = argparse.ArgumentParser(description='Prediction script')
+
+    # Input-output shape
+    parser.add_argument('--input_data_dir',
+                        type=str,
+                        default="",
+                        help="input data directory")
+    parser.add_argument('--model_dir',
+                        type=str,
+                        default="/oak/stanford/groups/jamesz/zqwu/iPSC_data/model_save/random_split/0-to-inf_random/",
+                        help="model weight directory")
+    parser.add_argument('--output_dir',
+                        type=str,
+                        default="",
+                        help="output directory")
+    parser.add_argument('--pred_target_day',
+                        type=int,
+                        default=18,
+                        help="prediction target end point")
+    if cli:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args("")
+    return args
+
+
 if __name__ == '__main__':
-    DATA_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/VALIDATION/line_142/ex1/0-to-0/'
-    MODEL_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/model_save/random_split/0-to-inf_random/'
-    GEN_TYPE = CustomGenerator # PairGenerator for 3 channel, CustomGenerator for 2 channel
+    args = parse_args()
+    data_dir = args.input_data_dir
+    model_dir = args.model_dir
+    pred_save_dir = args.output_dir
+    target_day = args.pred_target_day
 
-    SUPPORT_DATA_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/TRAIN/0-to-inf_continuous/'
-    SUPPORT_GEN_TYPE = PairGenerator
-
-    PRED_SAVE_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/VALIDATION/line_142/ex1/pred_to-18/'
-    input_transform = partial(augment_fixed_end, end=18)
+    input_transform = partial(augment_fixed_end, end=target_day)
     input_filter = filter_for_inf_predictor
+    gen_fn = CustomGenerator if '0-to-0' in data_dir else PairGenerator # PairGenerator for 3 channel, CustomGenerator for 2 channel
 
-    valid_gen = get_data_gen(DATA_DIR, GEN_TYPE, batch_size=8)
-    model = get_model(MODEL_DIR)
-    collect_preds(valid_gen, model, PRED_SAVE_DIR, input_transform=input_transform, input_filter=input_filter)
-
-    # PRED_SAVE_DIR = '/oak/stanford/groups/jamesz/zqwu/iPSC_data/VALIDATION/line_142/ex1/pred_to-18_withsupp/'
-    # valid_gen = get_data_gen(DATA_DIR, GEN_TYPE, batch_size=2)
-    # support_gen = get_data_gen(SUPPORT_DATA_DIR, SUPPORT_GEN_TYPE, batch_size=6)
-    # collect_preds(valid_gen, model, PRED_SAVE_DIR, input_transform=input_transform, input_filter=input_filter, support_gen=support_gen)
+    valid_gen = get_data_gen(data_dir, gen_fn, batch_size=8)
+    model = get_model(model_dir)
+    collect_preds(valid_gen, model, pred_save_dir, input_transform=input_transform, input_filter=input_filter)
